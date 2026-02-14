f304d80e44d2d8c53495bbb8d740102d3069534b
Bhaveena Panneerselvam <bhaveenaps@gmail.com>
2026-02-14 12:29:09 +0530

    Implement Coin Change II solution using dynamic programming

---
+++ Coin Change II.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int dp[]= new int[amount+1];
+        dp[0]=1;
+        for(int x:coins)
+        {
+            for(int i=x;i<amount+1;i++)
+            {
+                dp[i]+=dp[i-x];
+            }
+        }
+        return dp[amount];
+    }
+}
